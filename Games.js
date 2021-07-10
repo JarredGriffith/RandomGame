0dfb504ab79c48ba2e1197d9cad63d75ef2532ca
JarredGriffith <Jarredgriffith29@gmail.com>
2021-07-10 13:18:44 -0700

    saving

---
+++ Games.js
@@ -0,0 +1,79 @@
+//array of games
+var arr = ["DbyD", "Rogue Company",
+    "Halo", "Rainbow", "COD", "Sandstorm", "Left 4 Dead", "GTFO", "Starwars", "Dead Island", "Gauntlet", "Phasmophobia", "Minecraft", ""
+];
+
+// photo for games 
+const photos = [{
+        game: "COD",
+        src: "https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Black_Ops_Cold_War.jpeg/220px-Black_Ops_Cold_War.jpeg",
+        width: "220",
+        height: "243"
+    },
+    {
+        game: "Rogue Company",
+        src: "https://cdn2.unrealengine.com/roco-egs-basegame-portraitproduct-1200x1600-1200x1600-491632859.jpg",
+        width: "200",
+        height: "243"
+    },
+    {
+        game: "DbyD",
+        src: "https://hb.imgix.net/372ca69b75119a0340cc2ba7712f47e3731c3eae.jpeg?auto=compress,format&fit=crop&h=353&w=616&s=8fca3a4424ab273bbf8f4d1778d8110d",
+        width: "260",
+        height: "243"
+    },
+    {
+        game: "Halo",
+        src: "https://cdn.cloudflare.steamstatic.com/steam/apps/1064271/capsule_616x353.jpg?t=1594746017",
+        width: "370",
+        height: "243"
+    },
+    {
+        game: "Left 4 Dead",
+        src: "https://cdn.cloudflare.steamstatic.com/steam/apps/500/capsule_616x353.jpg?t=1587582567",
+        width: "370",
+        height: "243"
+    },
+    {
+        game: "Starwars",
+        src: "https://images-na.ssl-images-amazon.com/images/I/81ZOuiDjEfL._SL1500_.jpg",
+        width: "243",
+        height: "243"
+    },
+    {
+        game: "Rainbow",
+        src: "https://store.ubi.com/dw/image/v2/ABBS_PRD/on/demandware.static/-/Sites-masterCatalog/en_US/dw64173a87/images/large/56c494ad88a7e300458b4d5a.jpg?sw=341&sh=450&sm=fit",
+        width: "170",
+        height: "243"
+    },
+    {
+        game: "Sandstorm",
+        src: "https://cdn.focus-home.com/admin/games/insurgency:_sandstorm/meta/og_image.jpg",
+        width: "486",
+        height: "243"
+    },
+    {
+        game: "Dead Island",
+        src: "https://www.g4less.com/images/product/10757_11.jpg",
+        width: "486",
+        height: "243"
+    },
+    {
+        game: "GTFO",
+        src: "https://o.aolcdn.com/images/dims?resize=1200%2C630&crop=1200%2C630%2C0%2C0&quality=95&image_uri=https%3A%2F%2Fs.yimg.com%2Fuu%2Fapi%2Fres%2F1.2%2FSsPF.SCHeg_VT4ntBIVJUQ--%7EB%2FaD05OTk7dz0xNjAwO2FwcGlkPXl0YWNoeW9u%2Fhttps%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fresize%3D2000%252C2000%252Cshrink%26image_uri%3Dhttps%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2019-12%2Fa387f110-1b8f-11ea-9fd6-c7ea8dd5f399%26client%3Da1acac3e1b3290917d92%26signature%3D7eb0ba96ddcbcf137482c527665e06725b9715ab&client=amp-blogside-v2&signature=4fc52da74e1887c0c2c2f75b8eac815e65986a11",
+        width: "486",
+        height: "243"
+    },
+    {
+        game: "Minecraft",
+        src: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png",
+        width: "170",
+        height: "243"
+    },
+    {
+        game: "Phasmophobia",
+        src: "https://cdn.cloudflare.steamstatic.com/steam/apps/739630/capsule_616x353.jpg?t=1609602804",
+        width: "340",
+        height: "243"
+    }
+];
---
+++ index.js
@@ -0,0 +1,44 @@
+let slot = document.querySelector('.slot'); 
+console.log(photos[0].game)
+let timeOutNumer = ()=> {
+    return Math.random() * (4000 - 2000) + 2000;
+}
+
+let allDays = photos.length; 
+  let firstDay = 0; 
+  let change = document.querySelector('.slot').innerHTML; 
+
+  changeDay = function() {
+     // console.log(change)
+      document.querySelector('.slot').innerHTML = photos[firstDay].game;
+    firstDay = (firstDay + 1) % allDays;
+  }
+  function ohNo() {
+    slot.classList.remove("off"); 
+        slot.classList.add("on"); 
+    ohYeah = setInterval(changeDay, 100);//must match css animation
+    setTimeout(()=> {
+        clearInterval(ohYeah)
+        slot.classList.remove("on");
+    slot.classList.add("off");
+    } ,timeOutNumer());
+  };
+
+// let allGames = game.length; 
+// let firstGame = 0; 
+// hange = document.querySelector('.slot').innerHTML;
+// console.log(document.querySelector('.slot').innerHTML)
+
+let button = document.querySelector('.button').addEventListener("click",()=> {
+   if(slot.classList.contains("on")){
+//    slot.classList.remove("on")
+//     slot.classList.add("off")
+    
+    }
+    else {
+        // slot.classList.remove("off"); 
+        // slot.classList.add("on"); 
+         ohNo(); 
+    }
+}); 
+
